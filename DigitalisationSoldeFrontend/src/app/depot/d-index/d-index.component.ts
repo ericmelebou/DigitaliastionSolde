a949dff8b8ee486bd5284c8b23c071615c32be53
jacks <jacksossou9@gmail.com>
2023-08-23 08:30:50 +0000

    up to date

--- DigitalisationSoldeFrontend/src/app/depot/d-index/d-index.component.ts
+++ DigitalisationSoldeFrontend/src/app/depot/d-index/d-index.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-d-index',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./d-index.component.scss']
 })
 export class DIndexComponent {
+  displayedColumns: string[] = ['name', 'date'];
+
+  // Exemple de données statiques
+  dataSource = new MatTableDataSource([
+    { name: 'Dossier 1', date: '2023-08-25' },
+    { name: 'Dossier 2', date: '2023-08-26' },
+    { name: 'Dossier 3', date: '2023-08-27' },
+    // Ajoutez plus de données si nécessaire
+  ]);
 
 }
--- DigitalisationSoldeFrontend/src/app/depot/d-layout/d-layout.component.ts
+++ DigitalisationSoldeFrontend/src/app/depot/d-layout/d-layout.component.ts
@@ -11,19 +11,10 @@ import { ModalComponent } from '../modal/modal.component';
 export class DLayoutComponent {
 
   openModal() {
-    
+
 
   }
 
-  displayedColumns: string[] = ['name', 'date'];
-
-  // Exemple de données statiques
-  dataSource = new MatTableDataSource([
-    { name: 'Dossier 1', date: '2023-08-25' },
-    { name: 'Dossier 2', date: '2023-08-26' },
-    { name: 'Dossier 3', date: '2023-08-27' },
-    // Ajoutez plus de données si nécessaire
-  ]);
 
 
 
